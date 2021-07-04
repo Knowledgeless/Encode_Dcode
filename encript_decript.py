ecabb5154116a4808173f3a8123422e70482b9ba
Knowledgeless <nazmulhossain914@gmail.com>
2021-07-05 02:57:05 +0600

    updated choice and print text of choice 2

--- encript_decript.py
+++ encript_decript.py
@@ -4,16 +4,15 @@ from methods import base64 as b64, md5
 
 if __name__ == "__main__":
     print("""
-            Chose An Option
+            Choose An Option
     ------------------------------
         1. Text => Base64
         2. Base64 => Text
-        3. Text => md5
-        4. md5 => Text 
+        3. Text => md5 
     """)
 
-    choise = int(input("Option No: "))
-    if choise == 1:
+    choice = int(input("Option No: "))
+    if choice == 1:
         user = input("Your Text: ")
         en = b64.Encode(user)
         print('''
@@ -21,15 +20,15 @@ if __name__ == "__main__":
             Base64 Text:    {}
         '''.format(user, en._bs64()))
 
-    elif choise == 2:
+    elif choice == 2:
         user = input("Your Text: ")
         dc = b64.Decode(user)
         print('''
-            Given Text:     {}
-            Base64 Text:    {}
+            Base64 Text:     {}
+            Plain Text:      {}
         '''.format(user, dc.base64()))
 
-    elif choise == 3:
+    elif choice == 3:
         user = input("Your Text: ")
         en = md5.Encode(user)
         hash_name = ["md5", "sha1", "sha224", "sha256", "sha384", "sha512"]
